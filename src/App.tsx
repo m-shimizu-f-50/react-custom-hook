bf24eb5e33893024c8fff3fc811d5e0ce6ebcdab
m-shimizu <62657297+m-shimizu-ring@users.noreply.github.com>
2022-02-27 12:34:06 +0900

    カスタムフックの実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,15 @@
-import axios from "axios";
-import { useState } from "react";
 import { UserCard } from "./components/UserCard";
+import { useAllUsers } from "./hooks/useAllUsers";
 import "./styles.css";
-import { User } from "./types/api/user";
-import { userProfile } from "./types/userProfile";
 
 export default function App() {
-  const [userProfiles, serUserProfiles] = useState<Array<userProfile>>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const onClickFetchUser = () => {
-    setLoading(true);
-    setError(false);
-
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        // 必要なものだけdataに変換して格納する
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.suite}`
-        }));
-
-        serUserProfiles(data);
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+  const onClickFetchUser = () => getUsers();
   return (
     <div className="App">
       <button onClick={onClickFetchUser}>データ取得</button>
       <br />
+      {/* エラーが起きた場合と読み込み時のLoddingの実装 */}
       {error ? (
         <p style={{ color: "red" }}>データの取得に失敗しました。</p>
       ) : loading ? (
